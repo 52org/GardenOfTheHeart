aca377fdd16aaac34a786dfd3220fe0899fafdea
bluewow <dogseen@gmail.com>
2022-11-27 22:14:22 +0900

    feat: cors 업데이트

--- src/main/java/com/letter/plant/config/WebMvcConfig.java
+++ src/main/java/com/letter/plant/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000",
-                "http://xxx")
+                "https://52org.github.io/garden-of-the-heart/")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
