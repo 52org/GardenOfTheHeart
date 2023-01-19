b91dfe8babba4a19ab7d8c94e9570226686e8b31
kibum414 <kibum414@naver.com>
2023-01-19 21:11:42 +0900

    remove: all

--- src/main/java/com/letter/plant/application/letter/repository/LetterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.letter.plant.application.letter.repository;
-
-import com.letter.plant.application.letter.domain.Letter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LetterRepository extends JpaRepository<Letter, Long> {
-}
