a1214e0f4f62262371541a9c051aaf7ed20147ce
kibum414 <kibum414@naver.com>
2023-01-19 22:01:24 +0900

    init: create project

---
+++ src/main/java/com/garden/heart/GardenOfTheHeartApplication.java
@@ -0,0 +1,13 @@
+package com.garden.heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenOfTheHeartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenOfTheHeartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garden/heart/GardenOfTheHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.heart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenOfTheHeartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
