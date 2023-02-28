9a6d620505e285fbdf4d85f2f51623bdea946697
kibum414 <kibum414@naver.com>
2023-02-28 13:48:16 +0900

    chore: change antMatchers

--- src/main/java/com/_52org/gardenoftheheart/security/config/SecurityConfig.java
+++ src/main/java/com/_52org/gardenoftheheart/security/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/check", "/stat/**", "/garden/join", "/garden/login", "/seed/**", "/letter/send").permitAll()
+                .antMatchers("/check", "/stat/**", "/garden/join", "/garden/login", "/plant/**", "/letter").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
